2638cc632fa86f4ed17d599a36dfb6d001b484ae
Uk4sz3nk0 <gerdzio53@gmail.com>
2023-10-15 13:13:18 +0200

    Project base init

---
+++ src/main/java/com/engineers/backendprojectapp/BackendProjectAppApplication.java
@@ -0,0 +1,13 @@
+package com.engineers.backendprojectapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProjectAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProjectAppApplication.class, args);
+	}
+
+}
