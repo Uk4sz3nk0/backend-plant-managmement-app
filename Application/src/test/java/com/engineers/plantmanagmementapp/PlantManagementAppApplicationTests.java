2d75045a225a7731fe6bfb5a2d79b2529a126f26
Uk4sz3nk0 <gerdzio53@gmail.com>
2023-10-17 22:42:20 +0200

    Project init

---
+++ Application/src/test/java/com/engineers/plantmanagmementapp/PlantManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engineers.plantmanagmementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
