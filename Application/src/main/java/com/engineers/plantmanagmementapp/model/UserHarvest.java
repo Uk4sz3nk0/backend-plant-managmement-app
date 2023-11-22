1673907cc0a297ecdd6d4aeffc18e3292560769d
Uk4sz3nk0 <gerdzio53@gmail.com>
2023-11-22 12:43:09 +0100

    [FEATURE] - Adding harvests data

---
+++ Application/src/main/java/com/engineers/plantmanagmementapp/model/UserHarvest.java
@@ -0,0 +1,43 @@
+package com.engineers.plantmanagmementapp.model;
+
+import com.engineers.plantmanagmementapp.enums.PlantName;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+/**
+ * UserHarvest - Entity class that represents detailed harvest info by user in specific day
+ * Created on: 2023-11-22
+ * @author Łukasz Wodniak
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user_harvests")
+public class UserHarvest {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    private PlantName plantName;
+    private Integer row;
+    private LocalDateTime harvestStart;
+    private LocalDateTime harvestEnd;
+    @ManyToOne
+    @JoinColumn(name = "harvest_id")
+    private Harvest harvest;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "sector_id")
+    private Area sector;
+}
---
+++ Application/src/main/resources/db/migration/V3__Adding-harvests.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS harvests (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    season INTEGER NOT NULL,
+    date DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS user_harvests(
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    plant_name varchar(60) NOT NULL,
+    row INTEGER NOT NULL,
+    harvest_start TIMESTAMPTZ,
+    harvest_end TIMESTAMPTZ,
+    harvest_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    sector_id BIGINT NOT NULL,
+    FOREIGN KEY (harvest_id) REFERENCES harvests(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (sector_id) REFERENCES areas(id)
+);
