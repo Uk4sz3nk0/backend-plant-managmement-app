40ab3d472e943883b2ac057fc44fe971d915215f
Łukasz Wodniak <gerdzio53@gmail.com>
2023-11-28 19:31:57 +0100

    [FEATURE] - Adding harvests data

--- Application/src/test/java/com/engineers/plantmanagmementapp/controllers/HarvestsControllerTests.java
+++ Application/src/test/java/com/engineers/plantmanagmementapp/controllers/HarvestsControllerTests.java
@@ -1,50 +1,82 @@
 package com.engineers.plantmanagmementapp.controllers;
 
 import com.engineers.plantmanagmementapp.PlantManagerAppTests;
+import com.engineers.plantmanagmementapp.model.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.File;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @PlantManagerAppTests
 public class HarvestsControllerTests extends AbstractControllerTests {
 
-    @Test
-    void shouldAddHarvest() throws Exception {}
+    private static final String DIRECTORY_PREFIX = "harvests" + File.separator;
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    void shouldAddUserHarvest() throws Exception {}
+    void shouldAddHarvest() throws Exception {
+
+    }
 
     @Test
-    void shouldDeleteHarvest() throws Exception {}
+    void shouldAddUserHarvest() throws Exception {
+
+    }
 
     @Test
-    void shouldDeleteUserHarvest() throws Exception {}
+    void shouldDeleteHarvest() throws Exception {
+    }
 
     @Test
-    void shouldEditHarvest() throws Exception {}
+    void shouldDeleteUserHarvest() throws Exception {
+    }
 
     @Test
-    void shouldEditUserHarvest() throws Exception {}
+    void shouldEditHarvest() throws Exception {
+    }
 
     @Test
-    void shouldGetHarvestById() throws Exception {}
+    void shouldEditUserHarvest() throws Exception {
+    }
 
     @Test
-    void shouldGetHarvests() throws Exception {}
+    void shouldGetHarvestById() throws Exception {
+    }
 
     @Test
-    void shouldGetHarvestsByDate() throws Exception {}
+    void shouldGetHarvests() throws Exception {
+    }
 
     @Test
-    void shouldGetHarvestsBySeason() throws Exception {}
+    void shouldGetHarvestsByDate() throws Exception {
+    }
 
     @Test
-    void shouldGetHarvestsInDateRange() throws Exception {}
+    void shouldGetHarvestsBySeason() throws Exception {
+    }
 
     @Test
-    void shouldGetUserHarvestById() throws Exception {}
+    void shouldGetHarvestsInDateRange() throws Exception {
+    }
 
     @Test
-    void shouldGetUserHarvests() throws  Exception {}
+    void shouldGetUserHarvestById() throws Exception {
+    }
 
     @Test
-    void shouldGetUserHarvestByPlantation() throws Exception {}
+    void shouldGetUserHarvests() throws Exception {
+    }
+
+    @Test
+    void shouldGetUserHarvestByPlantation() throws Exception {
+    }
 }
--- Application/src/test/java/com/engineers/plantmanagmementapp/services/ServicesAbstractTests.java
+++ Application/src/test/java/com/engineers/plantmanagmementapp/services/ServicesAbstractTests.java
@@ -4,45 +4,5 @@ import org.junit.jupiter.api.Test;
 
 public abstract class ServicesAbstractTests {
 
-    @Test
-    void shouldAddHarvest() {}
 
-    @Test
-    void shouldAddUserHarvest() {}
-
-    @Test
-    void shouldDeleteHarvest() {}
-
-    @Test
-    void shouldDeleteUserHarvest() {}
-
-    @Test
-    void shouldEditHarvest() {}
-
-    @Test
-    void shouldEditUserHarvest() {}
-
-    @Test
-    void shouldGetHarvestById() {}
-
-    @Test
-    void shouldGetHarvests() {}
-
-    @Test
-    void shouldGetHarvestsByDate() {}
-
-    @Test
-    void shouldGetHarvestsBySeason() {}
-
-    @Test
-    void shouldGetHarvestsInDateRange() {}
-
-    @Test
-    void shouldGetUserHarvestById() {}
-
-    @Test
-    void shouldGetUserHarvests() {}
-
-    @Test
-    void shouldGetUserHarvestByPlantation() {}
 }
