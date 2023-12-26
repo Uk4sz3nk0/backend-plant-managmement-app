f71272a2871f4208181ab4b2108a4f386c932514
Łukasz Wodniak <gerdzio53@gmail.com>
2023-12-26 23:57:17 +0100

    [FEATURE] - Adding searching employee

--- Application/src/main/java/com/engineers/plantmanagmementapp/repository/UserRepository.java
+++ Application/src/main/java/com/engineers/plantmanagmementapp/repository/UserRepository.java
@@ -3,9 +3,12 @@ package com.engineers.plantmanagmementapp.repository;
 import com.engineers.plantmanagmementapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(final String email);
+
+    List<User> findAllByEmailContainingIgnoreCase(final String email);
 }
